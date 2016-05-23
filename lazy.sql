fe88bb86d8a4134ab92d6717118035aa4ece017f
Ryan Flynn <parseerror@gmail.com>
2016-05-23 09:58:56 -0400

    sandbox lazy.sql in its own schema

--- lazy.sql
+++ lazy.sql
@@ -2,75 +2,74 @@
 -- ref: https://hashrocket.com/blog/posts/materialized-view-strategies-using-postgresql
 
 
-drop schema if exists lazy cascade;
-create schema lazy;
+drop schema if exists test_lazy cascade;
+create schema test_lazy;
 
 
-drop table if exists accounts cascade;
-create table accounts(
+drop table if exists test_lazy.accounts cascade;
+create table test_lazy.accounts(
   name varchar primary key
 );
 
-insert into accounts (name)
+insert into test_lazy.accounts (name)
           select 'foo'
 union all select 'bar'
 union all select 'baz'
 ;
 
-
-drop table if exists transactions;
-create table transactions(
+drop table if exists test_lazy.transactions;
+create table test_lazy.transactions(
   id serial primary key,
-  name varchar not null references accounts
+  name varchar not null references test_lazy.accounts
     on update cascade
     on delete cascade,
   amount numeric(9,2) not null,
   post_time timestamptz not null
 );
 
-create index on transactions (name);
-create index on transactions (post_time);
+create index on test_lazy.transactions (name);
+create index on test_lazy.transactions (post_time);
 
 
-create table lazy.account_balances_mat(
-  name varchar primary key references accounts
+create table test_lazy.account_balances_mat(
+  name varchar primary key references test_lazy.accounts
     on update cascade
     on delete cascade,
   balance numeric(9,2) not null default 0,
   expiration_time timestamptz not null
 );
 
-create index on lazy.account_balances_mat (balance);
-create index on lazy.account_balances_mat (expiration_time);
+create index on test_lazy.account_balances_mat (balance);
+create index on test_lazy.account_balances_mat (expiration_time);
 
-insert into lazy.account_balances_mat(name, expiration_time)
+insert into test_lazy.account_balances_mat(name, expiration_time)
 select name, '-Infinity'
-from accounts;
+from test_lazy.accounts;
 
 
 
-create function lazy.account_insert() returns trigger
+create function test_lazy.account_insert() returns trigger
   security definer
   language plpgsql
 as $$
   begin
-    insert into lazy.account_balances_mat(name, expiration_time)
+    insert into test_lazy.account_balances_mat(name, expiration_time)
       values(new.name, 'Infinity');
     return new;
   end;
 $$;
 
-create trigger lazy_account_insert after insert on accounts
-    for each row execute procedure lazy.account_insert();
+create trigger lazy_account_insert after insert on test_lazy.accounts
+    for each row execute procedure test_lazy.account_insert();
 
 
-create function lazy.transaction_insert()
+create function test_lazy.transaction_insert()
   returns trigger
   security definer
   language plpgsql
 as $$
   begin
-    update lazy.account_balances_mat
+    update test_lazy.account_balances_mat
     set expiration_time=new.post_time
     where name=new.name
       and new.post_time < expiration_time;
@@ -78,17 +77,17 @@ as $$
   end;
 $$;
 
-create trigger lazy_transaction_insert after insert on transactions
-    for each row execute procedure lazy.transaction_insert();
+create trigger lazy_transaction_insert after insert on test_lazy.transactions
+    for each row execute procedure test_lazy.transaction_insert();
 
 
-create function lazy.transaction_update()
+create function test_lazy.transaction_update()
   returns trigger
   security definer
   language plpgsql
 as $$
   begin
-    update lazy.account_balances_mat
+    update test_lazy.account_balances_mat
     set expiration_time = '-Infinity'
     where name in (old.name, new.name)
       and expiration_time <> '-Infinity';
@@ -97,17 +96,17 @@ as $$
   end;
 $$;
 
-create trigger lazy_transaction_update after update on transactions
-    for each row execute procedure lazy.transaction_update();
+create trigger lazy_transaction_update after update on test_lazy.transactions
+    for each row execute procedure test_lazy.transaction_update();
 
 
-create function lazy.transaction_delete()
+create function test_lazy.transaction_delete()
   returns trigger
   security definer
   language plpgsql
 as $$
   begin
-    update lazy.account_balances_mat
+    update test_lazy.account_balances_mat
     set expiration_time='-Infinity'
     where name=old.name
       and old.post_time <= expiration_time;
@@ -116,12 +115,12 @@ as $$
   end;
 $$;
 
-create trigger lazy_transaction_delete after delete on transactions
-    for each row execute procedure lazy.transaction_delete();
+create trigger lazy_transaction_delete after delete on test_lazy.transactions
+    for each row execute procedure test_lazy.transaction_delete();
 
 
-create function lazy.refresh_account_balance(_name varchar)
-  returns lazy.account_balances_mat
+create function test_lazy.refresh_account_balance(_name varchar)
+  returns test_lazy.account_balances_mat
   security definer
   language sql
 as $$
@@ -135,10 +134,10 @@ as $$
         min(post_time) filter (where current_timestamp < post_time),
         'Infinity'
       ) as expiration_time
-    from transactions
+    from test_lazy.transactions
     where name=_name
   )
-  update lazy.account_balances_mat
+  update test_lazy.account_balances_mat
   set balance = t.balance,
     expiration_time = t.expiration_time
   from t
@@ -147,37 +146,38 @@ as $$
 $$;
 
 
-create view lazy.account_balances as
+create view test_lazy.account_balances as
 select name, balance
-from lazy.account_balances_mat
+from test_lazy.account_balances_mat
 where current_timestamp < expiration_time
 union all
 select r.name, r.balance
-from lazy.account_balances_mat abm
-  cross join lazy.refresh_account_balance(abm.name) r
+from test_lazy.account_balances_mat abm
+  cross join test_lazy.refresh_account_balance(abm.name) r
 where abm.expiration_time <= current_timestamp;
 
 
 
-select * from lazy.account_balances_mat;
+select * from test_lazy.account_balances_mat;
 
-insert into transactions (name, amount, post_time)
+insert into test_lazy.transactions (name, amount, post_time)
           select 'foo', 1, timestamp '2013-01-01'
 union all select 'foo', 2, timestamp '2012-01-01'
 union all select 'bar', 3, timestamp '2014-01-01'
 ;
 
--- update transactions set amount=5 where name='bar';
+-- update test_lazy.transactions set amount=5 where name='bar';
 
 /*
-insert into lazy.account_balances_mat(name, expiration_time)
+insert into test_lazy.account_balances_mat(name, expiration_time)
 select name, '-Infinity'
-from accounts;
+from test_lazy.accounts;
 */
 
-select * from accounts;
-select * from transactions;
-select * from lazy.account_balances_mat;
-select * from lazy.account_balances;
---select * from lazy.account_balances where balance < 0;
+select * from test_lazy.accounts;
+select * from test_lazy.transactions;
+select * from test_lazy.account_balances_mat;
+select * from test_lazy.account_balances;
+--select * from test_lazy.account_balances where balance < 0;
 
+drop schema if exists test_lazy cascade;
