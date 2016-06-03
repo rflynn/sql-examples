64aac42c5b1893af6601e427a692518f372262df
Ryan Flynn <parseerror+github@gmail.com>
2016-06-03 00:15:48 -0400

    docker wait loop

--- run.sh
+++ run.sh
@@ -2,20 +2,24 @@
 
 # ref: https://github.com/sameersbn/docker-postgresql
 
-set -v
+#set -v
 
 # ensure postgresql container running
 docker run \
   --name postgresql \
-  -itd \
+  --rm \
   --publish 5432:5432 \
   --volume /srv/docker/postgresql:/var/lib/postgresql \
-  sameersbn/postgresql:9.4-21
+  sameersbn/postgresql:9.4-21 &> /dev/null &
 
-sleep 2
+# wait for server or timeout...
+timeout=$(($(date +%s) + 10))
+while [ $(date +%s) -lt $timeout ] && ! docker exec -i postgresql sudo -u postgres psql < <(echo select 1) &> /dev/null
+do
+    sleep 1
+done
 
 docker exec -i postgresql sudo -u postgres psql -a < /dev/stdin
 
 docker stop postgresql
-docker rm postgresql
 
