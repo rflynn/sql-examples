7dc3e9d0a5ba4dd60a21a282158de74ed1b12b72
Ryan Flynn <parseerror@gmail.com>
2016-07-04 17:50:12 -0400

    update run and lazy so they work...

--- run.sh
+++ run.sh
@@ -2,20 +2,28 @@
 
 # ref: https://github.com/sameersbn/docker-postgresql
 
-set -v
+set -o xtrace
+set -o errexit
+set -o pipefail
 
-# ensure postgresql container running
+
+cleanup() {
+    docker stop postgresql
+}
+trap cleanup EXIT
+
+which docker-machine && eval $(docker-machine env)
+docker-machine ip default # ensure this doesn't crash
+
 docker run \
-  --name postgresql \
-  -itd \
-  --publish 5432:5432 \
-  --volume /srv/docker/postgresql:/var/lib/postgresql \
-  sameersbn/postgresql:9.4-21
+    --name postgresql \
+    --hostname postgresql94 \
+    --rm \
+    --publish 5432:5432 \
+    --volume /srv/docker/postgresql:/var/lib/postgresql \
+    --env 'DB_USER=dbuser' --env 'DB_PASS=dbuserpass' \
+    sameersbn/postgresql:9.4-21 &
 
-sleep 2
-
-docker exec -i postgresql sudo -u postgres psql -a < /dev/stdin
-
-docker stop postgresql
-docker rm postgresql
+until nc -w 0 $(docker-machine ip default) 5432 || [ $SECONDS -gt 10 ]; do sleep 1; done
+docker exec -i postgresql sudo -u postgres psql -a < $1
 
