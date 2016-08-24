a24c6bb69fb16a1519d4cdc6a62a4643bc7cac29
Ryan Flynn <parseerror@gmail.com>
2016-08-24 09:31:04 -0400

    schedule around weekends and holidays

--- date_schedule_around_weekends_and_holidays.sql
+++ date_schedule_around_weekends_and_holidays.sql
@@ -8,30 +8,47 @@ federal_holidays (date, descr) as (
         ('2016-12-26'::date, 'Christmas Day'   )
         -- ...
 ),
-days as (
+every_day as (
+    -- list every date between some finite date range
     select '2016-08-24'::date as d
     union all
-    select (d + interval '1 day')::date from days where d < now() + interval '1 month'
+    select (d + interval '1 day')::date from every_day where d < '2018-01-01'::date
 ),
 dows as (
+    -- calculate metadata about every_day
     select
         d,
         extract(dow from d) as dow,
         extract(dow from d) in (0, 6) as is_weekend,
         exists(select 1 from federal_holidays where date=d) as is_holiday
-    from days
-),
-unschedulable as (
-    select d from dows where is_weekend or is_holiday
+    from every_day
 ),
 schedulable as (
+    -- for each day, figure out the prev/next schedulable day
     select
         dows.d,
         dows.dow,
         dows.is_weekend,
         dows.is_holiday,
-        (select max(s.d) from dows s where s.d <= dows.d and not s.is_weekend and not s.is_holiday) as prev_schedulable,
         (select min(s.d) from dows s where s.d >= dows.d and not s.is_weekend and not s.is_holiday) as next_schedulable
     from dows
+),
+some_naive_monthly_schedule (d, n) as (
+    -- given a starting date, calculate the next N naive dates
+    -- use the 31st of the month as a test as it doesn't exist in many months and gives the least-intuitive results
+    -- NOTE: use (starting_date + n months) instead of (last_date + 1 month),
+    -- as months with fewer days like feb will trim the 31st down to 28th for all subsequent months
+    select '2016-10-31'::date, 1
+    union all
+    select ('2016-10-31'::date + (interval '1 month' * n))::date, n + 1
+    from some_naive_monthly_schedule where n <= 12
+),
+some_naive_monthly_shedule_adjusted as (
+    -- translate each naive day to the next scheduable day
+    select
+        snms.d,
+        s.next_schedulable
+    from some_naive_monthly_schedule snms
+    left join schedulable s on s.d = snms.d
 )
-select * from schedulable;
+select * from some_naive_monthly_shedule_adjusted;
